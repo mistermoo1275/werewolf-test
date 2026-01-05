8f0ba1a764b1c48219b3eca51ac5c36b2e82e78f
mistermoo1275 <mistermoo1275@gmail.com>
2026-01-05 14:38:25 +1100

    Update server.js

--- server.js
+++ server.js
@@ -10,7 +10,10 @@ const io = new Server(server);
 app.use(express.static(path.join(__dirname)));
 
 let players = [];
-let finishedCount = 0;
+let turnOrder = ['Werewolf', 'Seer', 'Villager', 'Gremlin'];
+let currentTurnIndex = 0;
+let votes = {}; 
+let skipVotes = new Set();
 
 io.on('connection', (socket) => {
     socket.on('joinGame', (username) => {
@@ -22,45 +25,77 @@ io.on('connection', (socket) => {
 
     socket.on('startGame', () => {
         if (players.length === 5) {
-            finishedCount = 0;
+            votes = {};
+            skipVotes.clear();
             const roles = ['Werewolf', 'Werewolf', 'Seer', 'Villager', 'Gremlin'].sort(() => Math.random() - 0.5);
-            players.forEach((p, i) => {
-                p.role = roles[i];
-                // Send role AND the list of all players to each person
-                io.to(p.id).emit('assignRole', { 
-                    role: p.role, 
-                    allPlayers: players.map(pl => pl.name) 
-                });
-            });
+            players.forEach((p, i) => { p.role = roles[i]; });
+            currentTurnIndex = 0;
+            sendTurnUpdate();
         }
     });
 
-    // Seer Logic
-    socket.on('seerAction', (targetIndex) => {
-        const target = players[targetIndex];
-        socket.emit('seerResult', `${target.name} is a ${target.role}`);
+    function sendTurnUpdate() {
+        const currentRoleGoal = turnOrder[currentTurnIndex];
+        io.emit('turnAnnouncement', currentRoleGoal);
+        players.forEach(p => {
+            if (p.role === currentRoleGoal) {
+                io.to(p.id).emit('yourTurnAction', { role: p.role, players: players.map(pl => pl.name) });
+            } else {
+                io.to(p.id).emit('waitTurn', currentRoleGoal);
+            }
+        });
+    }
+
+    socket.on('nextTurn', () => {
+        currentTurnIndex++;
+        if (currentTurnIndex < turnOrder.length) {
+            sendTurnUpdate();
+        } else {
+            io.emit('startDiscussion');
+        }
     });
 
-    // Gremlin Logic
+    socket.on('voteToSkip', () => {
+        skipVotes.add(socket.id);
+        io.emit('skipUpdate', skipVotes.size);
+        if (skipVotes.size === 5) {
+            io.emit('startVoting');
+        }
+    });
+
+    socket.on('submitVote', (targetName) => {
+        votes[targetName] = (votes[targetName] || 0) + 1;
+    });
+
+    socket.on('revealResult', () => {
+        let maxVotes = 0;
+        let winners = [];
+        for (let name in votes) {
+            if (votes[name] > maxVotes) { maxVotes = votes[name]; winners = [name]; }
+            else if (votes[name] === maxVotes) { winners.push(name); }
+        }
+
+        let werewolfCaught = false;
+        winners.forEach(winnerName => {
+            const player = players.find(p => p.name === winnerName);
+            if (player && player.role === 'Werewolf') werewolfCaught = true;
+        });
+
+        io.emit('finalReveal', {
+            outcome: werewolfCaught ? "VILLAGERS WIN!" : "WEREWOLVES WIN!",
+            truth: players.map(p => ({ name: p.name, role: p.role }))
+        });
+    });
+
+    socket.on('seerAction', (targetIdx) => {
+        socket.emit('seerResult', `${players[targetIdx].name} is a ${players[targetIdx].role}`);
+    });
+
     socket.on('gremlinAction', (indices) => {
-        const [idx1, idx2] = indices;
-        const temp = players[idx1].role;
-        players[idx1].role = players[idx2].role;
-        players[idx2].role = temp;
-        socket.emit('gremlinResult', "Roles swapped successfully!");
-    });
-
-    // Move to Morning
-    socket.on('playerFinished', () => {
-        finishedCount++;
-        if (finishedCount === 5) {
-            io.emit('morningPhase', players.map(p => ({ name: p.name, role: p.role })));
-        }
-    });
-
-    socket.on('disconnect', () => {
-        players = players.filter(p => p.id !== socket.id);
-        io.emit('playerListUpdate', players.map(p => p.name));
+        const [i1, i2] = indices;
+        const temp = players[i1].role;
+        players[i1].role = players[i2].role;
+        players[i2].role = temp;
     });
 });
 
